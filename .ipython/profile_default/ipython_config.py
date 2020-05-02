401899637dd83d66835d26f76bda16af188e097a
Julian Hess <jhess@broadinstitute.org>
2020-05-02 17:04:19 -0400

    Add line_profiler to ipython config

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -32,7 +32,9 @@
 #c.InteractiveShellApp.exec_lines = []
 
 ## A list of dotted module names of IPython extensions to load.
-#c.InteractiveShellApp.extensions = []
+c.InteractiveShellApp.extensions = [
+  'line_profiler'
+]
 
 ## dotted module name of an IPython extension to load.
 #c.InteractiveShellApp.extra_extension = ''
