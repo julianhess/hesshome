1d0dd06a05b1327ea2887f479b171b2c1908d1d4
Julian Hess <jhess@broadinstitute.org>
2025-08-19 21:17:09 +0000

    Update realpath

--- bin/realpath.c
+++ bin/realpath.c
@@ -1,23 +1,29 @@
+#include <errno.h>
 #include <limits.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*
- * usage: realpath <path>
+ * usage: realpath <path> [path [path [...]]]
  * completely expands all symlinks.  wrapper to system call realpath.
- * 2015-04-27//Julian Hess
  */
 
 int main(int argc, char** argv) {
-   char* rp;
-
    if(argc < 2) {
-      printf("Usage: realpath <path>\n");
+      printf("Usage: realpath <path> [path [path [...]]]\n");
       exit(1);
    }
 
-   if(!(rp = realpath(argv[1], rp))) {
-      printf("Error: %s does not exist.\n", argv[1]);
-      exit(1);
-   } else printf("%s\n", rp);
+   int ret = 0;
+   int i = 1;
+   char* rp = malloc(PATH_MAX);
+   while(i < argc) {
+      if(!(rp = realpath(argv[i], rp))) {
+         ret = errno;
+         fprintf(stderr, "realpath: error processing '%s': %s.\n", argv[i], strerror(ret));
+      } else printf("%s\n", rp);
+      i++;
+   }
+   return ret;
 }
